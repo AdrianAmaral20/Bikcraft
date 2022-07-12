6216e135ff73b8b07b12457d32fcc637b3521f84
AdrianAmaral20 <adri13amaral@gmail.com>
2022-07-12 00:12:35 -0300

    Adicionada galeria de fotos intuitiva com js, também adicionado animação na aba de contatos nas perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Ativar Links do Menu
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLinks(link) {
@@ -9,3 +11,56 @@ function ativarLinks(link) {
 }
 
 links.forEach(ativarLinks);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+  console.log(resposta);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de imagens
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
